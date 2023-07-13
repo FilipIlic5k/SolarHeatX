3c753b41c288f6111de5b215049f49cd86f73e70
Filip Ilic <filip_ilic@MacBook-Pro-von-Filip.local>
2023-07-13 14:04:07 +0200

    Basic Static Website with Webhooks implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include "cert.h"
 #include "firmwareFetch.h"
 #include "firmwareUpdate.h"
@@ -5,12 +6,144 @@
 #include "repeatedCall.h"
 #include "tempSensor.h"
 #include "wifiConnect.h"
+
 #include <Arduino.h>
 #include <HTTPClient.h>
 #include <HTTPUpdate.h>
 #include <WiFiClientSecure.h>
 
+#include "SPIFFS.h"
+#include <ArduinoJson.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
 String FirmwareVer = { "2.0" };
+
+#define HTTP_PORT 80
+
+// Init Server
+AsyncWebServer server(HTTP_PORT);
+AsyncWebSocket ws("/ws");
+
+struct Led {
+    // state variables
+    uint8_t pin;
+    bool on;
+
+    // methods
+    void update() { digitalWrite(pin, on ? HIGH : LOW); }
+};
+
+Led onboard_led = { LED_BUILTIN, false };
+
+void initSPIFFS()
+{
+    if (!SPIFFS.begin()) {
+        Serial.println("Cannot mount SPIFFS volume...");
+        while (1) {
+            onboard_led.on = millis() % 200 < 50;
+            onboard_led.update();
+        }
+    }
+}
+
+String processor(const String& var)
+{
+    /*
+      if (var == "STATE") {
+          if (digitalRead(led.on)) {
+              ledState = "ON";
+          } else {
+              ledState = "OFF";
+          }
+          return ledState;
+      } else if (var == "TEMP_SOLAR") {
+          return tempProcessor1();
+      } else if (var == "TEMP_PUFFER") {
+          return tempProcessor2();
+      } else if (var == "TEMP_BOILER") {
+          return tempProcessor3();
+      }*/
+    return "No Data";
+}
+
+// Send Html site
+void onRootRequest(AsyncWebServerRequest* request)
+{
+    request->send(SPIFFS, "/index.html", "text/html", false, processor);
+}
+
+void initWebServer()
+{
+    server.on("/", onRootRequest);
+    server.serveStatic("/", SPIFFS, "/");
+    server.begin();
+}
+
+void notifyClients()
+{
+    const uint8_t size = JSON_OBJECT_SIZE(10);
+    StaticJsonDocument<size> json;
+    /*
+    json["status"] = led.on ? "on" : "off";
+    json["TEMP_SOLAR"] = tempProcessor1();
+    json["TEMP_PUFFER"] = tempProcessor2();
+    json["TEMP_BOILER"] = tempProcessor3();
+    */
+    char data[500];
+    size_t len = serializeJson(json, data);
+    ws.textAll(data, len);
+}
+
+// Message Handler
+void handleWebSocketMessage(void* arg, uint8_t* data, size_t len)
+{
+    AwsFrameInfo* info = (AwsFrameInfo*)arg;
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+
+        const uint8_t size = JSON_OBJECT_SIZE(1);
+        StaticJsonDocument<size> json;
+        DeserializationError err = deserializeJson(json, data);
+        if (err) {
+            Serial.print(F("deserializeJson() failed with code "));
+            Serial.println(err.c_str());
+            return;
+        }
+
+        const char* action = json["action"];
+        if (strcmp(action, "toggle") == 0) {
+            // led.on = !led.on;
+            notifyClients();
+        }
+    }
+}
+
+void onEvent(
+    AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type, void* arg, uint8_t* data, size_t len)
+{
+
+    switch (type) {
+    case WS_EVT_CONNECT:
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        break;
+    case WS_EVT_DISCONNECT:
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        break;
+    case WS_EVT_DATA:
+        handleWebSocketMessage(arg, data, len);
+        break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+        break;
+    }
+}
+
+// Initializes a WebSocket
+void initWebSocket()
+{
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+}
 
 void setup()
 {
@@ -26,12 +159,18 @@ void setup()
 
     sensors.begin();
     fetchSensorAddresses();
+
+    delay(500);
+    initSPIFFS();
+    initWebSocket();
+    initWebServer();
 }
 
 void loop()
 {
 
     // repeatedCall();
+    notifyClients();
 
     // Send command to all the sensors for temperature conversion
     sensors.requestTemperatures();
@@ -51,5 +190,7 @@ void loop()
     }
 
     Serial.println("");
+
+    ws.cleanupClients();
     delay(1000);
 }
--- src/wifiConnect.h
+++ src/wifiConnect.h
@@ -7,9 +7,21 @@
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PW;
 
+// Set your Static IP address
+IPAddress local_IP(192, 168, 1, 187);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 254);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(8, 8, 8, 8); // optional
+IPAddress secondaryDNS(8, 8, 4, 4); // optional
+
 void connect_wifi()
 {
     Serial.println("Waiting for WiFi");
+    if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+        Serial.println("STA Failed to configure");
+    }
+
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
